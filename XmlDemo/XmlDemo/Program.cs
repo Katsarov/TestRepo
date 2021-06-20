07aff38a093ef74ed259897a153405e953295cc0
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-06-20 12:30:23 +0300

    gdgdg

---
+++ XmlDemo/XmlDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace XmlDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = File.ReadAllText("Data.xml");
+            Console.WriteLine(xml);
+        }
+    }
+}
